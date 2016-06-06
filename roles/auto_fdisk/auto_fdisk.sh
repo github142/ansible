056e86c4b2b61a421eeccac956cbc175bb12e732
github142 <pigsi@live.cn>
2016-06-06 14:41:03 +0800

    add

---
+++ roles/auto_fdisk/auto_fdisk.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+disk_dev=/dev/xvdb
+fdisk -S 56 $disk_dev << EOF
+n
+p
+1
+
++8G
+t
+82
+n
+p
+2
+
+
+wq
+EOF
+
+if [ $? -eq 0 ]; then
+    mkswap $disk_dev1
+    mkfs.ext4 $disk_dev2
+    echo "$disk_dev2             /opt                 ext4    defaults        0 0" >> /etc/fstab
+    echo "$disk_dev1             swap                 swap    defaults        0 0" >> /etc/fstab
+    mount -a
+  else
+    exit 1
+fi
